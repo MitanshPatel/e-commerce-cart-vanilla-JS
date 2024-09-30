1ab7034f5ef81c96f43a9d0b0d95c7486c987d82
Mitansh Patel <mitanshpatel288@gmail.com>
2024-09-30 22:58:12 +0530

    add delete cart functionality added

--- main.js
+++ main.js
@@ -148,3 +148,97 @@ mobile_prev.addEventListener('click', () => {
     key = (key === 0) ? img_list.length - 1 : (key - 1) % img_list.length;
     img_list[key].click();
 });
+
+
+//view cart
+const cart_btn = document.getElementById('cart-icon');
+
+cart_btn.addEventListener('click', () => {
+    var temp = document.getElementById('cart-modal').style.display;
+    if (temp === 'none' || temp === '') {
+        document.getElementById('cart-modal').style.display = 'flex';
+    } else {
+        document.getElementById('cart-modal').style.display = 'none';
+    }
+});
+
+// CRUD on cart
+const cart = [];
+const count_items = document.getElementById('count-items');
+const add_cart = document.getElementById('item-add');
+
+
+add_cart.addEventListener('click', () => {
+    if (count_items.textContent !== '0') {
+        const item = {
+            name: add_cart.getAttribute('item_name'),
+            price: add_cart.getAttribute('item_price'),
+            quantity: count_items.textContent,
+            img: main_img.src
+        };
+        let flag = false;
+        cart.forEach((i) => {
+            if (i.name === item.name) {
+                flag = true;
+                i.quantity = parseInt(i.quantity) + parseInt(item.quantity);
+                updateCart();
+            }
+        });
+        if (flag === false) {
+            cart.push(item);
+            updateCart();
+        }
+    }
+});
+
+const modal = document.getElementById('cart-modal');
+
+if (cart.length === 0) {
+    modal.innerHTML = "";
+    modal.innerHTML = `<h3>Cart</h3>
+         <div id="empty-cart-info">
+              <h4>Your cart is empty</h4>
+            </div>`;
+}
+
+const updateCart = () => {
+    if (cart.length === 0) {
+        modal.innerHTML = "";
+        modal.innerHTML = `<h3>Cart</h3>
+         <div id="empty-cart-info">
+              <h4>Your cart is empty</h4>
+            </div>`;
+    } else {
+        modal.innerHTML = "";
+        modal.innerHTML = `
+            <h3>Cart</h3>
+            <div id="cart-info">
+              <img src="${cart[0].img}" alt="">
+              <div class="cart-item-details">
+                <h4 style="font-weight: 400;">${cart[0].name}</h4>
+                <p>$ ${cart[0].price} x ${cart[0].quantity} <span>$ ${cart[0].quantity * cart[0].price}</span></p>
+              </div>
+              <button id="cart-item-delete">
+                <svg width="14" height="16" xmlns="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink">
+                  <defs>
+                    <path
+                      d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z"
+                      id="a" />
+                  </defs>
+                  <use fill="#C3CAD9" fill-rule="nonzero" xlink:href="#a" />
+                </svg>
+              </button>
+            </div>
+            <div id="checkout">
+              <button id="checkout-btn">Checkout</button>
+            </div>
+        `;
+
+        const delete_btn = document.getElementById('cart-item-delete');
+        delete_btn.addEventListener('click', () => {
+            cart.pop();
+            updateCart();
+        });
+    }
+};
